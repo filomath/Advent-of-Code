6a9bba32357a230ac31b2b1a539040d85627b764
Filip Rękawek <72637845+filomath@users.noreply.github.com>
2022-12-04 20:15:17 +0100

    Update main.py

--- 2022/day02/main.py
+++ 2022/day02/main.py
@@ -4,9 +4,9 @@
 
 def get_strategy(path_to_file): 
     # loads strategies to a list with following encoding: rock=1, paper=2, scissors=3
-    with open("input.txt") as file:
+    with open(path_to_file) as file:
         rounds = file.read().strip().split("\n")
-    letters = ["A","X","B","Y","C","Z"]
+    letters = ["A", "X", "B", "Y", "C", "Z"]
     return [list(map(lambda s: letters.index(s)//2+1, line.split(" "))) for line in rounds]
 
 def total_score(strategy, part):
@@ -14,7 +14,7 @@ def total_score(strategy, part):
     if part == 1: 
         return sum(map(lambda x: x[1] + (x[1]-x[0]+1)%3*3, strategy))
     else:
-        return sum(map(lambda x: (x[0]+x[1])%3+1 + (x[1]-1) * 3, strategy))
+        return sum(map(lambda x: (x[0]+x[1])%3+1 + (x[1]-1)*3, strategy))
 
 if __name__ == "__main__":
     strategy = get_strategy("input.txt")
